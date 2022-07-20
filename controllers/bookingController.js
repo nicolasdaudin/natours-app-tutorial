de66b3647dab3aed5ee43a576f173f38735d861d
nicdo77 <nicolas.daudin@gmail.com>
2022-07-20 10:43:03 +0200

    Fixed some bug in createBookingCheckout

--- controllers/bookingController.js
+++ controllers/bookingController.js
@@ -51,7 +51,7 @@ const createBookingCheckout = async (session) => {
   console.log('session', session);
   const tour = session.client_reference_id;
   const user = (await User.findOne({ email: session.customer_email })).id;
-  const price = session.line_items[0].amount / 100;
+  const price = session.amount_total / 100;
 
   console.log(tour, user, price);
 
